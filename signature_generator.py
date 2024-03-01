ba94e061af237837f50d4ac62ca11124bf0a4817
erick-sudo <erickocchieng766@gmail.com>
2024-03-01 07:23:33 +0300

    Jenga KEYS

--- signature_generator.py
+++ signature_generator.py
@@ -10,7 +10,7 @@ from Crypto.PublicKey import RSA
 payload = {
     "countryCode": "KE",
     "accountId": "0570182740362",
-    "date": "2024-01-30"
+    "date": "2024-01-31"
 }
 
 # message = f"{payload['accountId']}{payload['countryCode']}{payload['date']}".encode('utf-8')
@@ -21,22 +21,24 @@ digest.update(message)
 
 privateKey = False
 
-with open("privatekey.pem") as pfile:
+with open("jenga-private-key.pem") as pfile:
     privateKey = RSA.importKey(pfile.read())
 
 signer = PKCS1_v1_5.new(privateKey)
 sigBytes = signer.sign(digest)
 signBase64 = b64encode(sigBytes)
 
-headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer eyJhbGciOiJSUzUxMiJ9.eyJ0b2tlblR5cGUiOiJNRVJDSEFOVCIsImVudiI6IlVBVCIsImV4cCI6MTcwNjU5OTk3NiwiaWF0IjoxNzA2NTk5MDc2LCJhY2NvdW50IjoiN0NDNUNENzFCMTdDQ0NEOUNFNzcyQzZFNEE5Mzk5MzVGQUQwNUQ5MUVGNzI3MzkyM0ZDODNFOTc1ODk0NkY5NDI4MzU5Q0E5QjVFREUyNDkzRkUzMkQ1Q0UzQzI4NkI0UWxDbGkwNURCc0pKelRtZjdRSHNZK05ZOGdYSHBabXBFVjZ4bE5HeDNnbmZIMHBuSTc4QnYyWDhhcHBzcmNYcDE3M2svcE9RcXhUWE5qMHdQSVVJZFlUY0NpU2J3OWtVZUZRV0o5UXdRaW44K2FyQ0NpSzAyNVpvM3RpUUt3dmNtbmNSZzFZUHNQcUloVm1PcGJSU0xXN1QwdEdZdFZua0dUNHloTVhhVXI1d2dBMFlMT1E4RkJLdzFZYW1RMVVzOTFqMlg0NExHYmlia0I5Ukc4bHhpbWtmQlVBa3J6bUpibEgwTzRzbE91Y1NQMUhmUHF2RlpEVFhDNWJWOHdXajNNMmMxSXN3SmNuUUgxQ1lCaExGT3V1cTNhcmM3eEg3MnoxZDdTb0NzWXVyZDJjZ2RMNlY1aG1GVE53WFpVZ1VZdXFVSFcrRjFHRUxoRTN0RUh3d3htNFd1Wkc2WDN0aGFrUnhyTGlDNjNSU2RXRnJrUUVrY1VYcEdYT0g4OUNYRld4RGJLQlhqbjVVN3FYOWkxU2d3emFqdE8yYm15d2FiS2JEeC92bkpMalBnWVEwUjRzdmFBdERRY090RG9IUyJ9.JEWYZKm4hUWf9pxWMaGeZ-_r-46GUwBCaPqHLMF1Su93s4bo3wDVUBV2jEV0ggz3ai5xP-WRWqA-UMZla4G5nCpeR50yjdQ9pMPej_EW7Gi73DC9sXK8u015XuZzRECcpv-rfEbJ3bstT4EZsk93O4JLSw7CA9x60GvjvGSQD34A-A5xqHoxSBpf4BwOuyVIVdfIg81zvHhpXD527bQDj5hO7kzcRj4-diSUWOLiFKHpfKxkQlz0BjFSzp-6qKplTBGOMUBMHGH6godY7OIVsmEmm79KZZw5y_l95dS4g5I9Q-5viZTREr3jpZjyJ_Un3QJNtlDo1xWLBijjZBff7g',
-    'signature': signBase64
-}
+print(signBase64)
 
-params = {}
+# headers = {
+#     'Content-Type': 'application/json',
+#     'Authorization': 'Bearer eyJhbGciOiJSUzUxMiJ9.eyJ0b2tlblR5cGUiOiJNRVJDSEFOVCIsImVudiI6IlVBVCIsImV4cCI6MTcwNjY2MTI2MSwiaWF0IjoxNzA2NjYwMzYxLCJhY2NvdW50IjoiQ0I5QjIzM0JFOTMzMEVGMzY2MTRGMzMwQkE3NTg4RjFDNEE1OURDMDM0QjVFNkZFOERCOTdERTI5NkU0MzYyM0JERkQ4MDI1QjQzNUNFQTNERjEzRUU1QUFEODZBNkE1anZ4RTlFUm9pSXIrQlh2WEd1Q2lZKzZKaU5Eci9XVFM3ek5acURDb2VaOHNXbUFGcHlQWEJjNDFveDI0ZW5vdGpuNnRZSTB6ZGdKekpoYy9JTkNxOE02amx6RGhDMW5VYUdhRGxNM1JaWGFUcUZsRElZOXpHTExGRVlyZzNPaGgwOXdIWE55b0xQVzRPS1FwL25vR0lib2dZQU5DaHdBbkxiTG9oTWpSaVBOVm5LL0ZkbUlpaGw1VjhnZDJmazBMcENLK0swTzZEUzIwdXd5VDBrTVVrWm5EY3czNlA2dExoT1JwWnpWL21STlJ2RGNuTGExZlRJaEZFQzR2UE9UT1pBckhiN0I4WkYvL2JOaGpOTE9ySjdQVVcrVU9UTThhYldUb052Q2tlVHZBM3lJZVByNE9uUTRmTzlFWVhPSVM2bWwxOXBYK1RybUtuL0NGdStmTDFFZGJjcGxQTm1Ld1FKMnJ1S2pNaVlOOTMrNWR6UmFNOVgyc2JyNXdWaU9OK0J0OUpXeXBzY2VtQnpKN0diaytxcndJV0J4RDNETVRhNmFmdzJaUzN1dnhQTEtHNVpWYVZDU1c5c3hUaGZXciJ9.MnhI0SUHG4l3kg2JuCUcwV5fWG5tDoYyzzpIiBsMlEyAGldXg1sLxtbNnS6hM9NXTmZd3y_d3P28LqzvI0J7HatGiiP4wk45nxCHOJU9RQwsO6ScPK1FEZEzJoAlnjwWuzxB8ryEBFGyjcUuRBwmENaDkkiS84qC3uDRrQQr4appBmYodI6jeDJkvhv1-il2hAT8yWufnGELjVgEqzVlilzKrSEcbu7NBXyO6bLHeRxuQA_7s6_k99ffVtpLGljzTTnxYTTINYPhifJ7JAdMTkK3gKRYlVIJs3DIlmHFBpeszPHUqbY7fFCxcQ2DYMV0W_wh8jtK5Ml2Lrji9KhcCg',
+#     'signature': signBase64
+# }
 
-url = "https://uat.finserve.africa/v3-apis/account-api/v3.0/accounts/balances/KE/0570182740362"
-#url = 'https://uat.finserve.africa/v3-apis/account-api/v3.0/accounts/accountBalance/query'
-response = requests.get(url, headers=headers, data=json.dumps(payload))
-print(response.text)
+# params = {}
+
+# url = "https://uat.finserve.africa/v3-apis/account-api/v3.0/accounts/balances/KE/0570182740362"
+# #url = 'https://uat.finserve.africa/v3-apis/account-api/v3.0/accounts/accountBalance/query'
+# response = requests.get(url, headers=headers) #, data=json.dumps(payload))
+# print(response.text)
